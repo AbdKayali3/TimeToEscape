39d92995ec563ea453ef946ae9f73625abb9cbd8
unknown <akayali532@gmail.com>
2023-04-25 07:47:42 +0200

    adding arrows

--- js/test4-4.js
+++ js/test4-4.js
@@ -17,6 +17,7 @@ const speed = ClaculateUnit(0.25) / FPS; // unit per second
 const animationSpeed = 4; // animation_frames per second
 
 
+let DragableObjects = [];
 let ColisionObjects = [];
 let ActiveColisionObjects = [];
 
@@ -62,6 +63,7 @@ class GameObject {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.isReachable = false;
     }
 
     draw(color = "red", image = false, src = null) {
@@ -90,6 +92,20 @@ class Box extends GameObject {
     draw(color = "red", image = false, src = null) {
         super.draw(color, image, src);
     }
+}
+
+class Arrow extends GameObject {
+    constructor(x, y, width, height, direction) {
+        super(x, y, width, height);
+        this.isDragging = false;
+        this.direction = direction;
+        this.isreachable = true;
+    }
+
+    draw(color = "yellow", image = false, src = null) {
+        super.draw(color, image, src);
+    }
+
 }
 
 class Props extends GameObject {
@@ -165,6 +181,8 @@ class Player extends GameObject {
 class Enemy extends Player {
     constructor(x, y, width, height, direction = "up", isMoving = true) {
         super(x, y, width, height, direction, isMoving);
+        this.startTime = null;
+        this.time = 1;
         // this.show = false;
     }
 
@@ -173,11 +191,22 @@ class Enemy extends Player {
     }
 
     actiate() {
-        // watit for 3 seconds then show
-        setTimeout(() => {
-            this.show = true;
-        }, 3000);
+        this.tick();
     }
+
+    tick(timestamp) {
+        if (!this.startTime) this.startTime = timestamp;
+        let elapsedTime = timestamp - this.startTime;
+        // console.log("elapsedTime: " + elapsedTime + " " + this.startTime);
+        // console.log("time: " + this.time);
+        if (elapsedTime >= this.time * 1000) {
+          this.show = true;
+        //   console.log("show");
+        } else {
+        //   console.log("not show");
+          window.requestAnimationFrame((timestamp) => this.tick(timestamp));
+        }
+      }
 
     move() {
         super.move();
@@ -298,6 +327,9 @@ class Level {
             new Box(ClaculateUnit(10), ClaculateUnit(10), ClaculateUnit(3), ClaculateUnit(3)),
         ];
 
+        this.arrows = [
+            new Arrow(ClaculateUnit(15), ClaculateUnit(15), ClaculateUnit(3), ClaculateUnit(3), "down"),
+        ];
 
         this.player = new Player(ClaculateUnit(30), ClaculateUnit(21), ClaculateUnit(3), ClaculateUnit(3), "up");
         this.enemy = new Enemy(ClaculateUnit(30), ClaculateUnit(21), ClaculateUnit(3), ClaculateUnit(3), "up");
@@ -315,6 +347,14 @@ class Level {
             let box = this.boxes[i];
             ColisionObjects.push(box);
             ActiveColisionObjects.push(box);
+            DragableObjects.push(box);
+        }
+
+        for (let i = 0; i < this.arrows.length; i++) {
+            let arrow = this.arrows[i];
+            ColisionObjects.push(arrow);
+            ActiveColisionObjects.push(arrow);
+            DragableObjects.push(arrow);
         }
 
         for (let i = 0; i < this.props.length; i++) {
@@ -345,6 +385,15 @@ class Level {
                         box.isDragging = true; // start dragging box
                     }
                 }
+
+                // check if any arrow was clicked
+                for (let i = 0; i < this.arrows.length; i++) {
+                    let arrow = this.arrows[i];
+                    if (mouseX >= arrow.x && mouseX <= arrow.x + arrow.width &&
+                        mouseY >= arrow.y && mouseY <= arrow.y + arrow.height) {
+                        arrow.isDragging = true; // start dragging arrow
+                    }
+                }
             }
         });
         
@@ -356,6 +405,13 @@ class Level {
                         box.isDragging = false;
                     }
                 }
+
+                for (let i = 0; i < this.arrows.length; i++) {
+                    let arrow = this.arrows[i];
+                    if (arrow.isDragging) {
+                        arrow.isDragging = false;
+                    }
+                }
             }
         });
         
@@ -363,7 +419,8 @@ class Level {
             if (!this.alarmFlag) {
                 let mouseX = event.clientX - canvas.offsetLeft;
                 let mouseY = event.clientY - canvas.offsetTop;
-                this.moveBox(mouseX, mouseY);
+                // this.moveBox(mouseX, mouseY);
+                this.moveObject(mouseX, mouseY);
             }
         });
 
@@ -426,6 +483,11 @@ class Level {
         }
 
 
+        for (let i = 0; i < this.arrows.length; i++) {
+            let arrow = this.arrows[i];
+            arrow.draw();
+        }
+
 
         // zindex 3 zone//
         //////////////////
@@ -451,30 +513,31 @@ class Level {
 
     }
 
-    moveBox(mouseX, mouseY) {
-        for (let i = 0; i < this.boxes.length; i++) {
-            let box = this.boxes[i];
-            if (box.isDragging) {
-                // snap box position to grid
-                let snappedX = Math.floor((mouseX - box.width / 2) / gridSize) * gridSize;
-                let snappedY = Math.floor((mouseY - box.height / 2) / gridSize) * gridSize;
+    moveObject(mouseX, mouseY) {
+
+        for (let i = 0; i < DragableObjects.length; i++) {
+            let dragObj = DragableObjects[i];
+            if (dragObj.isDragging) {
+                // snap dragObj position to grid
+                let snappedX = Math.floor((mouseX - dragObj.width / 2) / gridSize) * gridSize;
+                let snappedY = Math.floor((mouseY - dragObj.height / 2) / gridSize) * gridSize;
     
-                // prevent box from moving through walls
+                // prevent dragObj from moving through walls
                 let canMoveX = true;
                 let canMoveY = true;
     
                 for (let i = 0; i < ColisionObjects.length; i++) {
-                    let obj1 = box;
+                    let obj1 = dragObj;
                     let obj2 = ColisionObjects[i];
                     if (obj1 !== obj2) {
                         if (snappedX < obj2.x + obj2.width &&
                             snappedX + obj1.width > obj2.x &&
                             snappedY < obj2.y + obj2.height &&
                             snappedY + obj1.height > obj2.y) {
-                            if (snappedX < obj2.x + obj2.width && snappedX + box.width > obj2.x) {
+                            if (snappedX < obj2.x + obj2.width && snappedX + dragObj.width > obj2.x) {
                                 canMoveY = false;
                             }
-                            if (snappedY < obj2.y + obj2.height && snappedY + box.height > obj2.y) {
+                            if (snappedY < obj2.y + obj2.height && snappedY + dragObj.height > obj2.y) {
                                 canMoveX = false;
                             }
                         }
@@ -482,10 +545,10 @@ class Level {
                 }
     
                 if (canMoveX) {
-                    box.x = snappedX;
+                    dragObj.x = snappedX;
                 }
                 if (canMoveY) {
-                    box.y = snappedY;
+                    dragObj.y = snappedY;
                 }
             }
         }
@@ -504,30 +567,58 @@ class Level {
         //check if the player is touching any element and stop if it is
         // the check should be only for elemts that is in the direction that the box os moving
         // if there is collision, the player should stop moving
-        for (let i = 0; i < ActiveColisionObjects.length; i++) {
-            let obj = ActiveColisionObjects[i];
+        for (let i = 0; i < ColisionObjects.length; i++) {
+            let obj = ColisionObjects[i];
             if (this.player.isMoving) {
                 if(SingleCollisionDetection(this.player, obj)) {
-                    if (this.player.direction === "left") {
-                        if (this.player.x < obj.x + obj.width) {
-                            this.player.isMoving = false;
+
+                    if(obj.isreachable) {
+
+                        if(obj.direction != this.player.direction) {
+
+                            if(Math.trunc(this.player.y) == obj.y || Math.trunc(this.player.x) == obj.x) {
+                                console.log("collision");
+                                this.player.direction = obj.direction;
+                            }
+
+                            // if(obj.direction == "left" || obj.direction == "right") {
+                            //     if(Math.trunc(this.player.y) == obj.y) {
+                            //         this.player.direction = obj.direction;
+                            //     }
+                            // }
+    
+                            // if(obj.direction == "up" || obj.direction == "down") {
+                            //     if(Math.trunc(this.player.x) == obj.x) {
+                            //         this.player.direction = obj.direction;
+                            //     }
+                            // }
+                            
+                        }
+
+                         
+                    } else {
+                        if (this.player.direction === "left") {
+                            if (this.player.x < obj.x + obj.width) {
+                                this.player.isMoving = false;
+                            }
+                        }
+                        if (this.player.direction === "right") {
+                            if (this.player.x + this.player.width > obj.x) {
+                                this.player.isMoving = false;
+                            }
+                        }
+                        if (this.player.direction === "up") {
+                            if (this.player.y < obj.y + obj.height) {
+                                this.player.isMoving = false;
+                            }
+                        }
+                        if (this.player.direction === "down") {
+                            if (this.player.y + this.player.height > obj.y) {
+                                this.player.isMoving = false;
+                            }
                         }
                     }
-                    if (this.player.direction === "right") {
-                        if (this.player.x + this.player.width > obj.x) {
-                            this.player.isMoving = false;
-                        }
-                    }
-                    if (this.player.direction === "up") {
-                        if (this.player.y < obj.y + obj.height) {
-                            this.player.isMoving = false;
-                        }
-                    }
-                    if (this.player.direction === "down") {
-                        if (this.player.y + this.player.height > obj.y) {
-                            this.player.isMoving = false;
-                        }
-                    }
+
                 }
             }
         }
@@ -544,9 +635,42 @@ class Level {
             Losing();
         }
 
+        for (let i = 0; i < ColisionObjects.length; i++) {
+            let obj = ColisionObjects[i];
+            if (this.enemy.isMoving) {
+
+                if(obj.isreachable) {
+
+                    if(SingleCollisionDetection(this.enemy, obj)) {
+
+                        if(obj.direction != this.enemy.direction) {
+
+                            if(Math.trunc(this.enemy.y) == obj.y || Math.trunc(this.enemy.x) == obj.x) {
+                                console.log("collision");
+                                this.enemy.direction = obj.direction;
+                            }
+                            
+                            // if(obj.direction == "left" || obj.direction == "right") {
+                            //     if(Math.trunc(this.enemy.y) == obj.y) {
+                            //         this.enemy.direction = obj.direction;
+                            //     }
+                            // }
+    
+                            // if(obj.direction == "up" || obj.direction == "down") {
+                            //     if(Math.trunc(this.enemy.x) == obj.x) {
+                            //         this.enemy.direction = obj.direction;
+                            //     }
+                            // }
+                            
+                        }
+                         
+                        
+                    }
+                }
+            }
+        }
+
     }
-
-
 
 }
 
@@ -561,7 +685,7 @@ function init() {
 
 function draw() {
     currentLevel.update();
-    setInterval(draw, 1000 / 60); // update at 60fps
+    setInterval(draw, frameRate); // update at 60fps
 }
 
 init();
